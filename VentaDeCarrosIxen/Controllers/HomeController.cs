cd51b9b7b935e501d3c531547f8671cb0385331a
EduardoIxen <tomas-1539@hotmail.com>
2016-04-15 01:57:54 -0600

    VentaDeCarrosIxen

---
+++ VentaDeCarrosIxen/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VentaDeCarrosIxen.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            if (Session["idUsuario"]!= null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login","Cuenta");
+            }
+        }
+    }
+}
---
+++ VentaDeCarrosIxen/Models/Carro.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace VentaDeCarrosIxen.Models
+{
+    public class Carro
+    {
+        [Key]
+        public int idCarro { get; set; }
+        public String modelo { get; set; }
+        public String marca { get; set; }
+        public String precio { get; set; }
+        public String color { get; set; }
+        public String combustible { get; set; }
+        public int kilometros { get; set; }
+        [Display(Name = "Año Fabricacion"), Required(ErrorMessage = "Año es oblligatoria")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime añoFabricacion { get; set; }
+        public String descripcion { get; set; }
+        public int cantidad { get; set; }
+        public virtual List<Archivo> archivos { get; set; }
+        public virtual List<Compra> compra { get; set; }
+
+    }
+}
